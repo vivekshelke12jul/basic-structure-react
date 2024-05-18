966e0bbd79232cfe7912e99e69c4966c60d1d5ce
vivek <vivekshelke12jul@gmail.com>
2024-05-19 02:22:47 +0530

    basic structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+const App = () => {
+  return (
+    <>
+      <Header />
+      <div>App</div>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+        <h1>My website</h1>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">blog</a></li>
+          </ul>
+        </nav>
+        <hr />
+
+    </header>
+)
+}
+
+export default Header
